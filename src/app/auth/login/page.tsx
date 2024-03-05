98c28531f19d445a26d0504d4802d0b80b78ccdf
catriel <madk3games@gmail.com>
2024-03-05 17:39:22 -0300

    mi primer commite

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Button } from "@radix-ui/themes";
+import { BookmarkIcon } from "@radix-ui/react-icons";
+const LoginPage = () => {
+  return (
+    <div>
+      LoginPage
+      <Button>
+        <BookmarkIcon width="16" height="16" /> Bookmark
+      </Button>
+    </div>
+  );
+};
+
+export default LoginPage;
